adb9fcf4e6dc2063880d8af7314ebc929034da84
Alex Macleod <alex@macleod.io>
2022-12-01 12:40:37 +0000

    Day 01 Part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,11 +1,29 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 fn main() {
     let input = include_str!("input");
 
-    let elves = input
-        .split("\n\n")
-        .map(|elf| elf.lines().map(|line| line.parse::<u64>().unwrap()));
+    let mut sums = input.split("\n\n").map(|elf| {
+        Reverse(
+            elf.lines()
+                .map(|line| line.parse::<u64>().unwrap())
+                .sum::<u64>(),
+        )
+    });
 
-    let largest = elves.map(|foods| foods.sum::<u64>()).max().unwrap();
+    let mut heap = BinaryHeap::from_iter(sums.by_ref().take(3));
 
-    println!("Part 1: {largest}");
+    for sum in sums {
+        let mut head = heap.peek_mut().unwrap();
+
+        if *head > sum {
+            *head = sum;
+        }
+    }
+
+    let largest = heap.iter().map(|sum| sum.0);
+
+    println!("Part 1: {}", largest.clone().max().unwrap());
+    println!("Part 2: {}", heap.iter().map(|sum| sum.0).sum::<u64>())
 }
