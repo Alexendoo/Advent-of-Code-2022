ae43dfe3f95034f8deaaf2578c8f63cca6e62f56
Alex Macleod <alex@macleod.io>
2022-12-04 12:16:58 +0000

    Day 04 Part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,19 +1,35 @@
 fn main() {
     let input = include_str!("input");
 
-    let ranges = input.lines().map(|line| {
-        let mut nums = line.split(['-', ',']);
-        let mut next = || nums.next().unwrap().parse::<u8>().unwrap();
+    let ranges = input
+        .lines()
+        .map(|line| {
+            let mut nums = line.split(['-', ',']);
+            let mut next = || nums.next().unwrap().parse::<u8>().unwrap();
 
-        (next()..=next(), next()..=next())
-    });
+            (next()..=next(), next()..=next())
+        })
+        .collect::<Vec<_>>();
 
-    let count = ranges
+    let part_1 = ranges
+        .iter()
         .filter(|(l, r)| {
             (l.contains(&r.start()) && l.contains(&r.end()))
                 || (r.contains(&l.start()) && r.contains(&l.end()))
         })
         .count();
 
-    println!("Part 1: {count}");
+    println!("Part 1: {part_1}");
+
+    let part_2 = ranges
+        .iter()
+        .filter(|(l, r)| {
+            l.contains(&r.start())
+                || l.contains(&r.end())
+                || r.contains(&l.start())
+                || r.contains(&l.end())
+        })
+        .count();
+
+    println!("Part 2: {part_2}");
 }
