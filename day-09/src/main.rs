ee19b1afd4713e2dade5c4890d0b6a11f5a5d874
Alex Macleod <alex@macleod.io>
2022-12-09 15:47:05 +0000

    Day 09 Part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,37 +1,44 @@
 use std::collections::HashSet;
 
-fn main() {
-    let input = include_str!("input");
+type Point = (i32, i32);
 
-    let mut head: (i32, i32) = (0, 0);
-    let mut tail = (0, 0);
+fn walk<const N: usize>(mut rope: [Point; N]) -> usize {
+    let mut visited = HashSet::from([(0, 0)]);
 
-    let mut visited = HashSet::from([tail]);
-
-    for line in input.lines() {
+    for line in include_str!("input").lines() {
         let (direction, steps) = line.split_once(' ').unwrap();
         let steps: usize = steps.parse().unwrap();
 
         for _ in 0..steps {
             match direction {
-                "U" => head.1 += 1,
-                "R" => head.0 += 1,
-                "D" => head.1 -= 1,
-                "L" => head.0 -= 1,
+                "U" => rope[0].1 += 1,
+                "R" => rope[0].0 += 1,
+                "D" => rope[0].1 -= 1,
+                "L" => rope[0].0 -= 1,
                 _ => unreachable!(),
             }
 
-            if head.0.abs_diff(tail.0) > 1 || head.1.abs_diff(tail.1) > 1 {
-                tail.0 += (head.0 - tail.0).signum();
-                tail.1 += (head.1 - tail.1).signum();
-            } else {
-                tail.0 += ((head.0 - tail.0) / 2).signum();
-                tail.1 += ((head.1 - tail.1) / 2).signum();
+            for i in 1..N {
+                let prev = rope[i - 1];
+                let curr = &mut rope[i];
+
+                if prev.0.abs_diff(curr.0) > 1 || prev.1.abs_diff(curr.1) > 1 {
+                    curr.0 += (prev.0 - curr.0).signum();
+                    curr.1 += (prev.1 - curr.1).signum();
+                } else {
+                    curr.0 += ((prev.0 - curr.0) / 2).signum();
+                    curr.1 += ((prev.1 - curr.1) / 2).signum();
+                }
             }
 
-            visited.insert(tail);
+            visited.insert(*rope.last().unwrap());
         }
     }
 
-    println!("Part 1: {}", visited.len());
+    visited.len()
+}
+
+fn main() {
+    println!("Part 1: {}", walk(<[Point; 2]>::default()));
+    println!("Part 2: {}", walk(<[Point; 10]>::default()));
 }
