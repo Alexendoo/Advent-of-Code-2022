7de604f2fd27075b9b5645209263d7213eae67ea
Alex Macleod <alex@macleod.io>
2022-12-09 15:52:41 +0000

    Simplify Day 09 Part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,8 +1,7 @@
 use std::collections::HashSet;
 
-type Point = (i32, i32);
-
-fn walk<const N: usize>(mut rope: [Point; N]) -> usize {
+fn walk<const N: usize>() -> usize {
+    let mut rope = [(0i32, 0i32); N];
     let mut visited = HashSet::from([(0, 0)]);
 
     for line in include_str!("input").lines() {
@@ -25,9 +24,6 @@ fn walk<const N: usize>(mut rope: [Point; N]) -> usize {
                 if prev.0.abs_diff(curr.0) > 1 || prev.1.abs_diff(curr.1) > 1 {
                     curr.0 += (prev.0 - curr.0).signum();
                     curr.1 += (prev.1 - curr.1).signum();
-                } else {
-                    curr.0 += ((prev.0 - curr.0) / 2).signum();
-                    curr.1 += ((prev.1 - curr.1) / 2).signum();
                 }
             }
 
@@ -39,6 +35,6 @@ fn walk<const N: usize>(mut rope: [Point; N]) -> usize {
 }
 
 fn main() {
-    println!("Part 1: {}", walk(<[Point; 2]>::default()));
-    println!("Part 2: {}", walk(<[Point; 10]>::default()));
+    println!("Part 1: {}", walk::<2>());
+    println!("Part 2: {}", walk::<10>());
 }
