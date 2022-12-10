5bbc69c76cbc6d14e5eb986040a247d57cabc78c
Alex Macleod <alex@macleod.io>
2022-12-10 12:56:38 +0000

    Day 10 Part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,9 +1,9 @@
-fn main() {
+fn cycles() -> impl Iterator<Item = i32> {
     let mut lines = include_str!("input").lines();
-    let mut x = 0;
-    let mut addx: Option<i32> = Some(1_i32);
+    let mut x = 1;
+    let mut addx: Option<i32> = None;
 
-    let iter = std::iter::from_fn(|| {
+    std::iter::from_fn(move || {
         let during = x;
 
         if let Some(added) = addx.take() {
@@ -13,13 +13,29 @@ fn main() {
         }
 
         Some(during)
-    });
+    })
+}
 
-    let part_1: i32 = iter
+fn main() {
+    let part_1: i32 = cycles()
         .enumerate()
-        .skip(20)
+        .skip(19)
         .step_by(40)
-        .map(|(i, x)| (i as i32) * x)
+        .map(|(i, x)| (i + 1) as i32 * x)
         .sum();
     println!("Part 1: {part_1}");
+
+    for (cycle, x) in cycles().enumerate() {
+        let draw_pos = cycle as i32 % 40;
+
+        if x.abs_diff(draw_pos) <= 1 {
+            print!("#");
+        } else {
+            print!(".");
+        }
+
+        if draw_pos == 39 {
+            println!();
+        }
+    }
 }
