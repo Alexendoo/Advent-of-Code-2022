45e34e37b9b13fac783de2ab8641f6028de2d7ba
Alex Macleod <alex@macleod.io>
2022-12-12 13:34:52 +0000

    Day 12 Part 2

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,16 +1,8 @@
 use pathfinding::directed::bfs::bfs;
+use std::mem;
 
-fn main() {
-    let input = include_str!("input");
-    let board: Vec<&[u8]> = input.lines().map(|line| line.as_bytes()).collect();
-
-    let start = board
-        .iter()
-        .enumerate()
-        .find_map(|(y, row)| row.iter().position(|&ch| ch == b'S').map(|x| (x, y)))
-        .unwrap();
-
-    let path = bfs(
+fn solve(board: &[&[u8]], start: (usize, usize), target: u8, reverse: bool) -> usize {
+    bfs(
         &start,
         |&(x, y)| {
             let elevation = |x: usize, y: usize| {
@@ -27,15 +19,34 @@ fn main() {
                     let x_next = x.checked_add_signed(dx)?;
                     let y_next = y.checked_add_signed(dy)?;
 
-                    let curr = elevation(x, y)?;
-                    let next = elevation(x_next, y_next)?;
+                    let mut curr = elevation(x, y)?;
+                    let mut next = elevation(x_next, y_next)?;
+                    if reverse {
+                        mem::swap(&mut curr, &mut next);
+                    }
 
                     (curr + 1 >= next).then_some((x_next, y_next))
                 })
         },
-        |&(x, y)| board[y][x] == b'E',
+        |&(x, y)| board[y][x] == target,
     )
-    .unwrap();
+    .unwrap()
+    .len()
+    .saturating_sub(1)
+}
 
-    println!("Part 1: {}", path.len() - 1);
+fn main() {
+    let input = include_str!("input");
+    let board: Vec<&[u8]> = input.lines().map(|line| line.as_bytes()).collect();
+
+    let pos = |target| {
+        board
+            .iter()
+            .enumerate()
+            .find_map(|(y, row)| row.iter().position(|&ch| ch == target).map(|x| (x, y)))
+            .unwrap()
+    };
+
+    println!("Part 1: {}", solve(&board, pos(b'S'), b'E', false));
+    println!("Part 2: {}", solve(&board, pos(b'E'), b'a', true));
 }
