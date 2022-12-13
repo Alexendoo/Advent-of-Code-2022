f06d70102f76c0d00706766505dc6545d7ff0026
Alex Macleod <alex@macleod.io>
2022-12-13 13:25:05 +0000

    Day 13 Part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -4,40 +4,57 @@ use serde::Deserialize;
 use serde_json::Deserializer;
 use std::cmp::Ordering;
 use std::iter::zip;
+use Packet::{Int, List};
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Eq)]
 #[serde(untagged)]
 enum Packet {
     Int(u32),
     List(Vec<Packet>),
 }
 
-fn compare(left_packet: &Packet, right_packet: &Packet) -> Ordering {
-    match (left_packet, right_packet) {
-        (Packet::Int(l), Packet::Int(r)) => l.cmp(&r),
-        (Packet::List(ls), Packet::List(rs)) => zip(ls, rs)
-            .map(|(l, r)| compare(l, r))
-            .find(|ordering| ordering.is_ne())
-            .unwrap_or_else(|| ls.len().cmp(&rs.len())),
-        (&Packet::Int(l), Packet::List(_)) => {
-            compare(&Packet::List(vec![Packet::Int(l)]), right_packet)
-        }
-        (Packet::List(_), &Packet::Int(r)) => {
-            compare(left_packet, &Packet::List(vec![Packet::Int(r)]))
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Int(l), Int(r)) => Ord::cmp(l, r),
+            (List(ls), List(rs)) => zip(ls, rs)
+                .map(|(l, r)| Ord::cmp(l, r))
+                .find(|ordering| ordering.is_ne())
+                .unwrap_or_else(|| ls.len().cmp(&rs.len())),
+            (&Int(l), List(_)) => Ord::cmp(&List(vec![Int(l)]), other),
+            (List(_), &Int(r)) => Ord::cmp(self, &List(vec![Int(r)])),
         }
     }
 }
 
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 fn main() {
-    let pairs = Deserializer::from_str(include_str!("input"))
+    let mut packets: Vec<Packet> = Deserializer::from_str(include_str!("input"))
         .into_iter::<Packet>()
         .map(Result::unwrap)
-        .array_chunks();
+        .collect();
 
-    let sum: usize = pairs
+    let sum: usize = packets
+        .iter()
+        .array_chunks()
         .enumerate()
-        .map(|(i, [l, r])| if compare(&l, &r).is_le() { i + 1 } else { 0 })
+        .map(|(i, [l, r])| if l < r { i + 1 } else { 0 })
         .sum();
 
     println!("Part 1: {sum}");
+
+    packets.sort_unstable();
+
+    let divider_a = List(vec![List(vec![Int(2)])]);
+    let divider_b = List(vec![List(vec![Int(6)])]);
+
+    let decoder_key = (packets.partition_point(|p| p < &divider_a) + 1)
+        * (packets.partition_point(|p| p < &divider_b) + 2);
+
+    println!("Part 2: {decoder_key}");
 }
