5b0b53cb2b16c07e5eabcf47bea2d26022d10d32
Alex Macleod <alex@macleod.io>
2022-12-17 19:16:22 +0000

    Day 17 Part 2

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,17 +1,14 @@
-type Board = [[bool; 7]; 2022 * 3];
-
-fn tower_height(board: &Board) -> i32 {
+fn tower_height(board: &[[bool; 7]]) -> i32 {
     board.partition_point(|row| row.contains(&true)) as i32
 }
 
-fn fall(board: &mut Board, rock: &[(i32, i32)], mut jets: impl Iterator<Item = i32>) {
+fn fall(board: &mut [[bool; 7]], rock: &[(i32, i32)], mut jets: impl Iterator<Item = i32>) {
     let mut offset_x = 2;
     let mut offset_y = tower_height(board) + 3;
 
     // print(board, rock, offset_x, offset_y);
 
     loop {
-
         let direction = jets.next().unwrap();
 
         let valid = rock.iter().all(|&(x, y)| {
@@ -24,7 +21,6 @@ fn fall(board: &mut Board, rock: &[(i32, i32)], mut jets: impl Iterator<Item = i
         if valid {
             offset_x += direction;
         }
-
 
         let valid = rock.iter().all(|&(x, y)| {
             let x = x + offset_x;
@@ -40,7 +36,6 @@ fn fall(board: &mut Board, rock: &[(i32, i32)], mut jets: impl Iterator<Item = i
                 let x = x + offset_x;
                 let y = y + offset_y;
 
-                debug_assert!(!board[y as usize][x as usize]);
                 board[y as usize][x as usize] = true;
             }
 
@@ -49,46 +44,69 @@ fn fall(board: &mut Board, rock: &[(i32, i32)], mut jets: impl Iterator<Item = i
     }
 }
 
+fn find_period(board: &[[bool; 7]]) -> (usize, usize) {
+    let end = tower_height(board) as usize;
+
+    for period_height in (0..end / 2).rev() {
+        for start in 0.. {
+            let mut chunks = board[start..end].chunks(period_height);
+            let Some(a) = chunks.next() else { break };
+            let Some(b) = chunks.next() else { break };
+
+            if a == b {
+                let rock_spaces = a.iter().flatten().filter(|&&b| b).count();
+                // 22 total spaces in the 5 rocks
+                let rocks_per_period = (rock_spaces / 22) * 5;
+
+                return (rocks_per_period, period_height);
+            }
+        }
+    }
+
+    unreachable!()
+}
+
 fn main() {
-    let mut jets = include_str!("input")
-        .trim()
+    let input = include_str!("input").trim();
+    let mut jets = input
         .bytes()
         .map(|byte| if byte == b'>' { 1 } else { -1 })
         .cycle();
 
-    let rocks = [
-        &[(0, 0), (1, 0), (2, 0), (3, 0)][..],
+    let rocks: [&[(i32, i32)]; 5] = [
+        &[(0, 0), (1, 0), (2, 0), (3, 0)],
         &[(1, 2), (0, 1), (1, 1), (2, 1), (1, 0)],
         &[(2, 2), (2, 1), (0, 0), (1, 0), (2, 0)],
         &[(0, 3), (0, 2), (0, 1), (0, 0)],
         &[(0, 1), (1, 1), (0, 0), (1, 0)],
     ];
+    let mut rock_cycle = rocks.into_iter().cycle();
 
-    let mut board: Board = [[false; 7]; 2022 * 3];
+    let mut board = vec![[false; 7]; 10_000_000];
 
-    for turn in 0..2022 {
-        fall(&mut board, rocks[turn % rocks.len()], &mut jets);
+    for _ in 0..2022 {
+        fall(&mut board, rock_cycle.next().unwrap(), &mut jets);
     }
 
     println!("Part 1: {}", tower_height(&board));
-}
 
-fn print(board: &Board, rock: &[(i32, i32)], offset_x: i32, offset_y: i32) {
-    let top = tower_height(board) + 7;
-
-    for y in (0..top).rev() {
-        print!("|");
-        for x in 0..7 {
-            if rock.contains(&(x - offset_x, y - offset_y)) {
-                print!("@");
-            } else if board[y as usize][x as usize] {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-
-        println!("|");
+    // Assume there's a repetition within the first 5000 rocks
+    for _ in 2022..5000 {
+        fall(&mut board, rock_cycle.next().unwrap(), &mut jets);
     }
-    println!("+-------+\n");
+
+    let pending = 1000000000000 - 5000;
+    let (rocks_per_period, period_height) = find_period(&board);
+
+    let fake_turns = pending / rocks_per_period;
+    let pending = pending % rocks_per_period;
+
+    for _ in 0..pending {
+        fall(&mut board, rock_cycle.next().unwrap(), &mut jets);
+    }
+
+    println!(
+        "Part 2: {}",
+        tower_height(&board) as usize + fake_turns * period_height
+    );
 }
