ce1d37f5518d2c334a259993a1f273f08dbf9f96
Alex Macleod <alex@macleod.io>
2022-12-21 14:20:22 +0000

    Day 21 Part 2

--- day-21/src/main.rs
+++ day-21/src/main.rs
@@ -6,26 +6,50 @@ enum Job {
     Op(&'static str, char, &'static str),
 }
 
-fn solve(monkey: &str, jobs: &HashMap<&str, Job>) -> i64 {
+fn solve(monkey: &str, jobs: &HashMap<&str, Job>) -> Option<i64> {
     match jobs[monkey] {
-        Job::Val(val) => val,
-        Job::Op(l, op, r) => {
-            let l = solve(l, jobs);
-            let r = solve(r, jobs);
+        Job::Val(val) => Some(val),
+        Job::Op(lhs, op, rhs) => {
+            let lhs = solve(lhs, jobs)?;
+            let rhs = solve(rhs, jobs)?;
 
             match op {
-                '+' => l + r,
-                '-' => l - r,
-                '/' => l / r,
-                '*' => l * r,
+                '+' => lhs.checked_add(rhs),
+                '-' => lhs.checked_sub(rhs),
+                '/' => lhs.checked_div(rhs),
+                '*' => lhs.checked_mul(rhs),
                 _ => unreachable!(),
             }
         }
     }
 }
 
+fn contains_humn(monkey: &str, jobs: &HashMap<&str, Job>) -> bool {
+    if monkey == "humn" {
+        return true;
+    }
+
+    match jobs[monkey] {
+        Job::Val(_) => false,
+        Job::Op(l, _, r) => contains_humn(l, jobs) || contains_humn(r, jobs),
+    }
+}
+
+fn binary_search(mut start: i64, mut end: i64, mut f: impl FnMut(i64) -> bool) -> i64 {
+    while start < end {
+        let mid = start + (end - start) / 2;
+        if f(mid) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+
+    start
+}
+
 fn main() {
-    let jobs = include_str!("input")
+    let mut jobs = include_str!("input")
         .lines()
         .map(|line| {
             let op = match line[6..].parse() {
@@ -41,5 +65,26 @@ fn main() {
         })
         .collect();
 
-    println!("Part 1: {}", solve("root", &jobs));
+    println!("Part 1: {}", solve("root", &jobs).unwrap());
+
+    let Job::Op(l, _, r) = jobs["root"] else { unreachable!() };
+
+    let (human_branch, target) = if contains_humn(l, &jobs) {
+        (l, solve(r, &jobs).unwrap())
+    } else {
+        (r, solve(l, &jobs).unwrap())
+    };
+
+    let initial = solve(human_branch, &jobs).unwrap();
+    jobs.insert("humn", Job::Val(0));
+    let zero = solve(human_branch, &jobs).unwrap();
+
+    let direction = zero.cmp(&initial);
+
+    let humn = binary_search(0, i64::MAX, |i| {
+        jobs.insert("humn", Job::Val(i));
+        solve(human_branch, &jobs).cmp(&Some(target)) == direction
+    });
+
+    println!("Part 2: {humn}");
 }
